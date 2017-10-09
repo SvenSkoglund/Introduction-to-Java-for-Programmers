4af7cda47faf519526752d1ec76d3f2af3c732a4
SvenSkoglund <32058317+SvenSkoglund@users.noreply.github.com>
2017-10-09 13:04:40 +0700

    :+1:

---
+++ SourceCodes/src/ch12/ShapeTester.java
@@ -0,0 +1,25 @@
+/*Write a tester program that contains a main() method that creates an array of three Shapes.
+Store two Rectangles and a Circle in the array. Loop through the array, printing out the area of
+each shape.*/
+
+package ch12;
+
+public class ShapeTester {
+
+	public static void main(String[] args) {
+
+		Shape1[] testShape = new Shape1[3];
+		Rectangle1 rec1 = new Rectangle1(1, 1, 2, 2);
+		Rectangle1 rec2 = new Rectangle1(1, 1, 6, 8);
+		Circle1 circ = new Circle1(1, 1, 12);
+
+		testShape[0] = rec1;
+		testShape[1] = rec2;
+		testShape[2] = circ;
+		
+		for (int i = 0; i < testShape.length; i++) {
+			System.out.println(testShape[i].getArea());
+		}
+	}
+
+}
