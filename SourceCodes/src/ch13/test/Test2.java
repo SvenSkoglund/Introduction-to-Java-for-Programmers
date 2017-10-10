314697e903ec37fe55edd97f5a3a6c417746b7c8
SvenSkoglund <32058317+SvenSkoglund@users.noreply.github.com>
2017-10-10 13:48:55 +0700

    :+1:

---
+++ SourceCodes/src/ch13/test/Test2.java
@@ -0,0 +1,14 @@
+/*Create a package called animal with two public classes, Dog and Cat. Create a Java application,
+within another package, that creates both a Dog and a Cat object. Test without using import, then
+again with the import statement*/
+package ch13.test;
+import ch13.animal.*;
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cat snowflake = new Cat();
+		Dog crystal = new Dog();
+	}
+
+}
