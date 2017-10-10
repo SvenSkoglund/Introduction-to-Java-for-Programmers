512ab8d65f723694797b0cf86115793a469d980e
SvenSkoglund <32058317+SvenSkoglund@users.noreply.github.com>
2017-10-10 15:52:25 +0700

    :+1:

--- SourceCodes/src/ch14/BadAgeException.java
+++ SourceCodes/src/ch14/BadAgeException.java
@@ -1,3 +1,8 @@
+/*Create an exception class called BadAgeException. Modify the Person class to contain an age
+field. Add an int argument to the Person constructor for the age. If the age given to the constructor
+is less than 0 or greater than 120, throw the BadAgeException. Modify the UsePerson class to
+test this constructor*/
+
 package ch14;
 
 public class BadAgeException extends Exception{
--- SourceCodes/src/ch14/Person.java
+++ SourceCodes/src/ch14/Person.java
@@ -1,3 +1,9 @@
+/*Create an exception class called BadAgeException. Modify the Person class to contain an age
+field. Add an int argument to the Person constructor for the age. If the age given to the constructor
+is less than 0 or greater than 120, throw the BadAgeException. Modify the UsePerson class to
+test this constructor*/
+
+
 package ch14;
 
 
