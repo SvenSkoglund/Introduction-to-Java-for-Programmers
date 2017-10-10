ebe07044288c5fdef862142ef35c0df1dbdec318
SvenSkoglund <32058317+SvenSkoglund@users.noreply.github.com>
2017-10-10 13:52:53 +0700

    :+1:

--- SourceCodes/src/ch13/animal/Jackal.java
+++ SourceCodes/src/ch13/animal/Jackal.java
@@ -1,7 +1,10 @@
+/*Add a non-public class called Jackal to your animal package. Try creating a Jackal object in
+your application.*/
 package ch13.animal;
 
 class Jackal {
 	String voice = "MEHAAHAH";
+
 	public Jackal() {
 		System.out.println(voice);
 	}
---
+++ SourceCodes/src/ch13/test/Test3.java
@@ -0,0 +1,16 @@
+/*Add a non-public class called Jackal to your animal package. Try creating a Jackal object in
+your application.*/
+package ch13.test;
+
+import ch13.animal.*;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cat snowflake = new Cat();
+		Dog crystal = new Dog();
+		// Not visible Jackal jack = new Jackal();
+	}
+
+}
